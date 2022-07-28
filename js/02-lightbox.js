1092c503cffda833bee6efc60eac49592fc47d9d
Daniel Prandzioch <prandziochdaniel32@gmail.com>
2022-07-28 18:43:22 +0200

    koniec

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const galleryItem = galleryItems
+	.map(
+		(galleryItem) => `<div class="gallery__item">
+<a class="gallery__item" href="${galleryItem.original}">
+  <img
+    class="gallery__image"
+    src="${galleryItem.preview}"
+    alt="${galleryItem.description}"
+  />
+</a>
+</div>`
+	)
+  .join("");
+  
+  gallery.insertAdjacentHTML("beforeend", galleryItem);
+ 
 console.log(galleryItems);
+  
+  var lightbox = new SimpleLightbox(".gallery a", {
+		captionDelay: 250,
+		captionsData: "alt",
+	});
+	
+    
+
